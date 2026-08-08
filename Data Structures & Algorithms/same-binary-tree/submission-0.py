ffb119b1f979a2c056f71442e08197cabc166109
michael-l3 <136485633+michael-l3@users.noreply.github.com>
2026-08-07 22:51:21 -0500

    Update: same-binary-tree - submission-0

--- Data Structures & Algorithms/same-binary-tree/submission-0.py
+++ Data Structures & Algorithms/same-binary-tree/submission-0.py
@@ -16,4 +16,4 @@ class Solution:
         if p.val != q.val: 
             return False 
         
-        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
