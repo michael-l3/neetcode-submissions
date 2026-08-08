8ff1d6515b3c14be2dec6eb1c0ee4bf37fb7780e
michael-l3 <136485633+michael-l3@users.noreply.github.com>
2026-08-07 22:28:30 -0500

    Update: depth-of-binary-tree - submission-0

--- Data Structures & Algorithms/depth-of-binary-tree/submission-0.py
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.py
@@ -10,7 +10,7 @@ class Solution:
         if not root: 
             return 0 
         
-        left = self.maxDepth(root.left)
-        right = self.maxDepth(root.right)
+        left = self.maxDepth(root.left) 
+        right = self.maxDepth(root.right)  
 
         return 1 + max(left,right)
