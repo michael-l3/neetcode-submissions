6217f12784835285f40bdd2a12970011e7846f9c
michael-l3 <136485633+michael-l3@users.noreply.github.com>
2026-08-08 16:03:23 -0500

    Update: subtree-of-a-binary-tree - submission-0

--- Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.py
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.py
@@ -18,12 +18,13 @@ class Solution:
             if p.val != q.val: 
                 return False 
             
-            return isSameTree(p.left,q.left) and isSameTree(p.right,q.right) 
+            return isSameTree(p.left,q.left) and isSameTree(p.right,q.right)
         
         if not root: 
             return False 
         
         if isSameTree(root,subRoot): 
-            return True 
+            return True
         
         return self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot)
+        
