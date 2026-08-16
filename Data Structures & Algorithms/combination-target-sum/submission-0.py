c7b29cc4d5d467e7f659780264d979bc834dc477
michael-l3 <136485633+michael-l3@users.noreply.github.com>
2026-08-16 05:10:53 -0500

    Update: combination-target-sum - submission-0

--- Data Structures & Algorithms/combination-target-sum/submission-0.py
+++ Data Structures & Algorithms/combination-target-sum/submission-0.py
@@ -1,27 +1,27 @@
 class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
-        res = []  
+        res = [] 
         level = [] 
 
-        def dfs(i, currSum): 
-            #base case 
+        def dfs(index, currSum): 
             if currSum == target: 
                 res.append(level.copy())
                 return 
-            #if currSum is too large 
+            
             if currSum > target: 
                 return 
-            #out of bounds case
-            if i >= len(nums): 
-                return 
             
-            #choose the number at i 
-            level.append(nums[i])
-            #choose it again 
-            dfs(i,currSum+nums[i])
+            if index >= len(nums): 
+                return
+
+            #choose the number 
+            level.append(nums[index])
+            #choose number again 
+            dfs(index,currSum + nums[index])
+            #no longer need the number too larger or out of bounds 
             level.pop() 
-            #choose the next number with the previous currSum 
-            dfs(i+1,currSum)
-        
+            #next  number 
+            dfs(index + 1, currSum)
+
         dfs(0,0)
         return res
