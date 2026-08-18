14490833c741510bcd5c738d7c035c170abb39ba
michael-l3 <136485633+michael-l3@users.noreply.github.com>
2026-08-17 22:31:45 -0500

    Update: implement-prefix-tree - submission-0

--- Data Structures & Algorithms/implement-prefix-tree/submission-0.py
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -4,40 +4,34 @@ class PrefixTree:
         self.trie = {}
 
     def insert(self, word: str) -> None:
-        d = self.trie 
+        d = self.trie
 
-        #iterate though the characters of the word 
+        #dictionary of characters 
         for ch in word: 
-            #if it isnt in the trie then add it 
             if ch not in d: 
                 d[ch] = {}
-            #move into the next child node 
             d = d[ch]
         
-        #something has to mark the end of the word --> we will use '#'
-        d['#'] = True 
+        d["#"] = True
 
     def search(self, word: str) -> bool:
-        #simiar to insert 
         d = self.trie 
 
         for ch in word: 
             if ch not in d: 
                 return False 
-            d = d[ch] 
+            d = d[ch]
         
-        #signals the end of the word
         return '#' in d
 
     def startsWith(self, prefix: str) -> bool:
-        #similar to search 
         d = self.trie 
 
         for ch in prefix: 
             if ch not in d: 
                 return False 
-            d = d[ch] 
+            d = d[ch]
         
-        #signals the end of the word
-        return True 
+        return True
+        
         
