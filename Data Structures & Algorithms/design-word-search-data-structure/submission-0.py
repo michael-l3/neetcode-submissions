4fcae65594c6f43663242a88e18ade99b03b5c0a
michael-l3 <136485633+michael-l3@users.noreply.github.com>
2026-08-17 22:45:14 -0500

    Update: design-word-search-data-structure - submission-0

--- Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -1,7 +1,6 @@
 class WordDictionary:
 
     def __init__(self):
-        #initialize the trie 
         self.trie = {}
 
     def addWord(self, word: str) -> None:
@@ -14,22 +13,21 @@ class WordDictionary:
         
         d['#'] = True 
 
+
     def search(self, word: str) -> bool:
         d = self.trie 
 
-        #going to have to do dfs in order to iterate through the trie 
-
         def dfs(index, parent_node): 
-            #base case if index = len(word)
+            
             if index == len(word): 
-                return "#" in parent_node
-
+                return '#' in parent_node 
+            
             ch = word[index]
-            #to conditions a wild card condition and a regualr condition 
+
             if ch == '.': 
-                #checking the children in the parent node
+                #then we have a wildcard and can enter the childen bc this one doesnt matter it can be anything 
                 for child in parent_node: 
-                    if child != '#' and dfs(index + 1,parent_node[child]): 
+                    if child != '#' and dfs(index+1,parent_node[child]): 
                         return True 
                 return False 
             else: 
@@ -38,4 +36,4 @@ class WordDictionary:
                 return dfs(index+1,parent_node[ch])
         
         return dfs(0,d)
-
+        
