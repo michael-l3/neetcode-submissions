52d71dee7f91985d3018579613536ed9aa96b243
michael-l3 <136485633+michael-l3@users.noreply.github.com>
2026-08-17 20:52:51 -0500

    Update: design-word-search-data-structure - submission-0

--- Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -1,6 +1,7 @@
 class WordDictionary:
 
     def __init__(self):
+        #initialize the trie 
         self.trie = {}
 
     def addWord(self, word: str) -> None:
@@ -8,40 +9,33 @@ class WordDictionary:
 
         for ch in word: 
             if ch not in d: 
-                d[ch] = {} 
-            #move into it 
+                d[ch] = {}
             d = d[ch]
         
-        #because the dot is used as a wild card we want to use something else as the end of the word 
-        # instead of this : d['.'] = '.', we will use this to represent the end of a word
-        d['#'] = True
+        d['#'] = True 
 
     def search(self, word: str) -> bool:
         d = self.trie 
 
-        #now we will search using dfs 
-        def dfs(index, dictionary):  #the index that we are currently at in the word and the starting dictionary(we are nested)
-            #base case is if the index is equal to the amount of letters in the word 
-            if index == len(word): 
-                #make sure we are returning the last part 
-                return '#' in dictionary #-> returns True if it is there False if not
-            
-            #finding the character now 
-            ch = word[index]
+        #going to have to do dfs in order to iterate through the trie 
 
-            #wildCard case 
+        def dfs(index, parent_node): 
+            #base case if index = len(word)
+            if index == len(word): 
+                return "#" in parent_node
+
+            ch = word[index]
+            #to conditions a wild card condition and a regualr condition 
             if ch == '.': 
-                #then go through all of the characters in the child nodes that exist 
-                for child in dictionary: 
-                    if child != '#' and dfs(index+1,dictionary[child]): 
+                #checking the children in the parent node
+                for child in parent_node: 
+                    if child != '#' and dfs(index + 1,parent_node[child]): 
                         return True 
                 return False 
             else: 
-                if ch not in dictionary: 
+                if ch not in parent_node: 
                     return False 
-                return dfs(index+1,dictionary[ch]) 
+                return dfs(index+1,parent_node[ch])
         
         return dfs(0,d)
 
-
-        
