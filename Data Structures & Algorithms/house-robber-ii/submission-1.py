520a4e01b29c985b1cec9ff16d9718bdd0570f4f
michael-l3 <136485633+michael-l3@users.noreply.github.com>
2026-08-20 11:10:37 -0500

    Update: house-robber-ii - submission-1

--- Data Structures & Algorithms/house-robber-ii/submission-1.py
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -1,35 +1,34 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        #just two house rob on two different list one starts at +1 
-        maxRob2 = 0
-        if len(nums) == 1:
+        n = len(nums)
+
+        if n == 1: 
             return nums[0]
-        if len(nums) == 2: 
+        if n == 2: 
             return max(nums[0],nums[1])
 
         def rob1(nums):
-            #cant rob two adjacent houses
             maxProfit = 0 
+            n = len(nums)
 
-            if len(nums) == 1: 
+            if n == 1: 
                 return nums[0]
-            if len(nums) == 2: 
-                return max(nums[0],nums[1])
+            if n == 2 : 
+                return max(nums[0], nums[1])
             
             first = nums[0]
-            second = max(nums[0],nums[1])
+            second = max(nums[0], nums[1])
 
-            for i in range(2,len(nums)): 
-                curr = max(first + nums[i], second)
+            for i in range(2,n): 
+                curr = max(first + nums[i],second)
+                if curr > maxProfit: 
+                    maxProfit = curr
                 first = second 
                 second = curr 
-
-                maxProfit = max(first, second)
             
             return maxProfit 
         
-        first = rob1(nums[:len(nums) - 1])
+        first = rob1(nums[:n - 1])
         second = rob1(nums[1:])
 
         return max(first,second)
-        
