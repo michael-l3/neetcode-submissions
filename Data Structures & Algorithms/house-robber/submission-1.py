9a7820e90f93220fbe3fedd6276da0c143dd9947
michael-l3 <136485633+michael-l3@users.noreply.github.com>
2026-08-20 11:04:26 -0500

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        maxProfit = 0 
+        n = len(nums)
+
+        if n == 1: 
+            return nums[0]
+        if n == 2 : 
+            return max(nums[0], nums[1])
+        
+        first = nums[0]
+        second = max(nums[0], nums[1])
+
+        for i in range(2,n): 
+            curr = max(first + nums[i],second)
+            if curr > maxProfit: 
+                maxProfit = curr
+            first = second 
+            second = curr 
+        
+        return maxProfit
