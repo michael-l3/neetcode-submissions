3819fb2051b8f9566ab2182b715493bef0cda5d1
michael-l3 <136485633+michael-l3@users.noreply.github.com>
2026-09-16 23:02:13 -0500

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.heap = []
+        self.stk = []
+
+    def push(self, val: int) -> None:
+        heapq.heappush(self.heap,val)
+        self.stk.append(val)
+
+    def pop(self) -> None:
+        val = self.stk.pop() 
+
+        self.heap.remove(val)
+        heapq.heapify(self.heap)
+
+    def top(self) -> int:
+        return self.stk[-1]
+        
+
+    def getMin(self) -> int:
+        return self.heap[0]
